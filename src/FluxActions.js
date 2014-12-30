f6fccc7f57199107268797d206258a0ac341665b
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-12-29 20:24:14 -0500

    Create Tux Actions methods.

---
+++ src/FluxActions.js
@@ -0,0 +1,118 @@
+//require in instance of Flux Dispatcher
+var Dispatcher = require('./Dispatcher.js');
+
+//Actions OBJECT
+var Actions = {};
+
+//expose Dispatcher in case user needs it
+Actions.__Dispatcher__ = Dispatcher;
+
+//Actions.register FUNCTION
+//@param categoriesToRegister OBJECT [ALTERNATE FUNCTION: function will be directly registered to dispatcher]
+  //expected keys:
+    //ACTION:CATEGORY OBJECT: key will be an action category created with the function defined below, value will be an object
+      //expected keys:
+        //ACTION:VERBS FUNCTION: key wil be an action verb within the category of actions, value is function to call when associated action is dispatched.  function receives action payload OBJECT as input
+Actions.register = function (storeToRegister, categoriesToRegister) {
+  //if function is passed in, register that instead
+  if (typeof storeToRegister === 'function') return Dispatcher.register(storeToRegister);
+  //registers actions to dispatcher CB under object, invoked when corresponding action is passed in
+  //define listener
+  var listener = {};
+  //get categories
+  var categories = Object.keys(categoriesToRegister);
+  var catLength = categories.length;
+  //loop through categories
+  var category, action, actions, actLength, i, j;
+  for (i = 0; i < catLength; i++){
+    //loop through actions in category
+    category = categories[i];
+    actions = Object.keys(category);
+    actLength = actions.length;
+    for (j = 0; j < actLength; j++){
+      action = actions[i];
+      //get action string from Actions[category][action] store key in listener with value of cb
+      listener[Actions[category][action].type] = categoriesToRegister[category][action];
+    }
+  }
+  //register to Dispatcher
+  storeToRegister.__registerId__ = Dispatcher.register(function(payload){
+    //look for actionType in listener
+    var registeredAction = listener[payload.action.actionType];
+    //if found invoke cb CONTEXT: store, ARGUMENTS: body, [payload if needed]
+    if (registeredAction) return registeredAction.call(storeToRegister, payload.action.body, payload);
+  });
+};
+
+//Actions.createActionClass FUNCTION
+//@param actionClass OBJECT
+  //expected keys:
+    //category STRING: type of data actions will refer to, for example: 'messages', 'todos'
+    //source STRING: source of action
+    //actions ARRAY: verbs that will act on the category, for example: 'read', 'create', 'update', 'destroy'
+Actions.createActionClass = function (actionClass) {
+  //creates object under Actions at the key of the category
+  var category = actionClass.category;
+  //throw error if category has already been defined
+  if (Actions[category]) throw new Error('Action Category is already defined');
+  var actionCategory = Actions[category] = new ActionCategory(actionClass);
+  //return actionCategory
+  return actionCategory;
+};
+
+//ActionCategory CONSTRUCTOR
+//@param props OBJECT
+  //expected keys:
+    //name STRING
+    //source STRING
+    //actions STRING or ARRAY
+var ActionCategory = function (props) {
+  //grab props for use in function
+  var category = props.category;
+  var source = props.source;
+  //assign passed in properties to object
+  this.__category__ = category;
+  this.__source__ = source;
+  //loop through actions and bind a separate function for each
+  //bind actionType and assign actionType to action at key type
+  var action, actionType;
+  var propActions = props.actions;
+  var propActLength = propActions.length;
+  for (var i = 0; i < propActLength; i++) {
+    action = propActions[i];
+    //construct full action name from CATEGORY_ACTION
+    actionType = category + '_' + action;
+    this[action] = dispatchAction.bind(this, actionType, source);
+    this[action].type = actionType;
+  }
+
+};
+
+//ActionCategory.register FUNCTION
+//@param actionsToRegister OBJECT keys are action verbs, values are callbacks to be invoked when action is dispatched
+ActionCategory.prototype.register = function (storeToRegister, actionsToRegister) {
+  var categoriesToRegister = {};
+  //register this category with passed in actions
+  categoriesToRegister[this.__category__] = actionsToRegister;
+  Actions.register(storeToRegister, categoriesToRegister);
+};
+
+//dispatchAction FUNCTION
+//@param actionType STRING should be bound by ActionCategory, actionType of action to be dispatched
+//@param source STRING should be bound by ActionCategory, source of action to be dispatched
+//@param body OBJECT body of action to be dispatched
+var dispatchAction = function (actionType, source, body) {
+  //define action
+  var action = {
+    actionType: actionType,
+    body: body
+  };
+  //dispatch action and source
+  Dispatcher.dispatch({
+    source: source,
+    action: action
+  });
+};
+
+//export Actions
+module.exports = Actions;
