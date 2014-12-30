1dac7fc0fbd617f839147237e3615bb2b9cffd3c
spencer stebbins <sjfstebbins@gmail.com>
2014-12-29 20:02:38 -0500

    Add form validation component.

--- src/Tuxedo.js
+++ src/Tuxedo.js
@@ -7,4 +7,7 @@ var Flux = require('flux');
 var Tux = Object.create(React);
 Tux.Dispatcher = Flux.Dispatcher;
 
+//Tux Components
+Tux.components.form = require('react-validating-form');
+
 module.exports = Tux;
