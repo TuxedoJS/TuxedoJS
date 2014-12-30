82bc35b16ed3c3c1994079d19dfa56602a4d2e50
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-12-30 15:04:14 -0500

    Include Actions in Tuxedo.

--- jest-preprocessor.js
+++ jest-preprocessor.js
@@ -1,9 +1,10 @@
-// preprocessor.js
 'use strict';
+
 var ReactTools = require('react-tools');
+
 module.exports = {
   process: function(src) {
-    return ReactTools.transform(src,{
+    return ReactTools.transform(src, {
       harmony: true
     });
   }
