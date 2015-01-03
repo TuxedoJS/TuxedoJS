0477e304e1eaef15df4c27789c24747bfcd5942b
drabinowitz <dmitrirabinowitz+development@gmail.com>
2015-01-03 13:34:55 -0500

    Add comments to code.

--- src/TuxGetOwnerPropsMixin.js
+++ src/TuxGetOwnerPropsMixin.js
@@ -1,6 +1,9 @@
+//getOwnerPropsMixin OBJECT: adds mixin to React class that will attach the nearestOwner's ownerProps under the key of nearestOwnerProps
 module.exports = {
+  //add property under componentWillMount so that other lifecycle methods will be able to access it
   componentWillMount: function () {
     var owner = this._owner;
+    //if the owner is an OwnerComponent get its ownerProps, otherwise get its nearestOwnerProps
     if (owner) {
       if (owner.__tuxIsOwnerComponent__) {
         this.nearestOwnerProps = owner.ownerProps;
