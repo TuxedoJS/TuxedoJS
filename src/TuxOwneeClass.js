418b4843561fdcc1accf89b175ef6df51c63eaba
drabinowitz <dmitrirabinowitz+development@gmail.com>
2015-01-03 12:06:29 -0500

    Change mixin variable to camelCase and return React.createElement.

--- src/TuxOwneeClass.js
+++ src/TuxOwneeClass.js
@@ -1,18 +1,17 @@
 var React = require('react');
-var assign = require('object-assign');
-var GetOwnerPropsMixin = require('./TuxGetOwnerPropsMixin');
+var getOwnerPropsMixin = require('./TuxGetOwnerPropsMixin');
 var assign = require('object-assign');
 
 var mixinsToAdd, mixins;
 var createOwneeClass = function (owneeClassProps) {
-  mixinsToAdd = [GetOwnerPropsMixin];
+  mixinsToAdd = [getOwnerPropsMixin];
 
   mixins = owneeClassProps.mixins;
   if (mixins) {
     mixinsToAdd = mixinsToAdd.concat(mixins);
   }
 
-  React.createClass(assign({}, owneeClassProps, {
+  return React.createClass(assign({}, owneeClassProps, {
     mixins: mixinsToAdd
   }));
 
