ee0a781dc0457473a9f90bbd9683bff6ed5e53f5
drabinowitz <dmitrirabinowitz+development@gmail.com>
2015-01-04 10:56:44 -0500

    Add comments and clean up code.

--- src/TuxArchitectStores.js
+++ src/TuxArchitectStores.js
@@ -1,71 +1,108 @@
 'use strict';
 
+//architecture OBJECT: stores will register their outputs to this object so that store inputs can lookup those outputs later
 var architecture = {};
 
+//ArchitectChain CONSTRUCTOR FUNCTION: produces architectChain instances which allow stores to chain methods for defining their inputs and outputs
+//@param storeToArchitect: store OBJECT that inputs and outputs will be registered to
 var ArchitectChain = function (storeToArchitect) {
   this.storeToArchitect = storeToArchitect;
 };
 
-ArchitectChain.prototype._getRegisterId = function (inputOrStore) {
-  var storeToWaitFor, storeRegistrationId;
-  if (typeof inputOrStore === 'string') {
-    storeToWaitFor = architecture[inputOrStore];
-    if (!storeToWaitFor) {
-      throw new Error('store is waiting for an input: "' + inputOrStore + '" that no store outputs.  If this store needs no inputs than only call "itOutputs"');
-    }
-  } else {
-    storeToWaitFor = inputOrStore;
-  }
-  storeRegistrationId = storeToWaitFor.__registerId__;
-  if (storeRegistrationId) {
-    return storeRegistrationId;
-  } else {
-    throw new Error('store is waiting for a store that has not been registered to the dispatcher');
-  }
-};
+//prototype methods
+//itNeeds FUNCTION: registers the input(s) and/or store(s) that the store will need to waitFor, returns this architectChain instance to allow for method chaining, updates "and" method to allow chaining via conjuction
+//@param inputOrStore STRING: input string to be checked against the architecture object for the corresponding store [ALTERNATE OBJECT: if a store is passed in than lookup __registerId__ on the passed in store] [ALTERNATE ARRAY: can pass in an array of inputs and/or stores to be registered, method chaining will still function properly in this case]
 ArchitectChain.prototype.itNeeds = function (inputOrStore) {
-  var result = [];
+  //array of ids to register to the store
+  var waitForIds = [];
+  //if inputOrStore is array
   if (Array.isArray(inputOrStore)) {
     var inputOrStoreLength = inputOrStore.length;
+    //loop through inputOrStore array and push register id into waitForIds for each element in array
     for (var i = 0; i < inputOrStoreLength; i++) {
-      result.push(this._getRegisterId(inputOrStore[i]));
+      waitForIds.push(this._getRegisterId(inputOrStore[i]));
     }
   } else {
-    result.push(this._getRegisterId(inputOrStore));
+    //if inputOrStore is not an array push its corresponding register id into array
+    waitForIds.push(this._getRegisterId(inputOrStore));
   }
+  //if the storeToArchitect has already been architected at some point than add waitForIds array on to existing array
   var tuxArchitecture = this.storeToArchitect.__tuxArchitecture__;
   if (tuxArchitecture) {
-    tuxArchitecture = tuxArchitecture.concat(result);
+    tuxArchitecture = tuxArchitecture.concat(waitForIds);
   } else {
-    tuxArchitecture = result;
+    //if it has not been architected than just use waitForIds array
+    tuxArchitecture = waitForIds;
   }
+  //attach architecture array to storeToArchitect at the key of __tuxArchitecture__
   this.storeToArchitect.__tuxArchitecture__ = tuxArchitecture;
-  this.and = ArchitectChain.prototype.itNeeds;
+  //update "and" method so user can chain additional needed inputs with "and"
+  this.and = this.itNeeds;
+  //return this architectChain instance to enable method chaining
   return this;
 };
-ArchitectChain.prototype._registerOutputToArchitecture = function (output) {
-  if (architecture[output]) {
-    throw new Error('output: "' + output + '" is already registered to a store');
-  } else {
-    architecture[output] = this.storeToArchitect;
-  }
-};
+
+//itOutputs FUNCTION: registers the output(s) that the store will be mapped to in the architecture OBJECT, returns architectChain instance to allow for method chaining, updates "and" method to allow chaining via conjuction
+//@param output STRING: output STRING to which storeToArchitect will be mapped to on the architecture OBJECT [ALTERNATE ARRAY: can pass in an array of output STRINGs to which store will be mapped to on the architecture OBJECT]
 ArchitectChain.prototype.itOutputs = function (output) {
+  //if output is a string than register it to architecture OBJECT
   if (typeof output === 'string') {
     this._registerOutputToArchitecture(output);
   } else {
+    //if output is an array than map each string in array to architecture OBJECT
     var outputLength = output.length;
     for (var i = 0; i < outputLength; i++) {
       this._registerOutputToArchitecture(output[i]);
     }
   }
-  this.and = ArchitectChain.prototype.itOutputs;
+  //update "and" method so user can chain additional outputs with "and"
+  this.and = this.itOutputs;
+  //return this architectChain instance to enable method chaining
   return this;
 };
 
+//_getRegisterId FUNCTION: internal function,for getting the store __registerId__ from either the passed in store or the store mapped to the passed in input
+//@param inputOrStore STRING: input string to be checked against the architecture OBJECT for the corresponding store [ALTERNATE OBJECT: if a store is passed in then lookup __registerId__ on the passed in store]
+ArchitectChain.prototype._getRegisterId = function (inputOrStore) {
+  var storeToWaitFor, storeRegistrationId;
+  //if inputOrStore is a string
+  if (typeof inputOrStore === 'string') {
+    //lookup corresponding store in the architecture object
+    storeToWaitFor = architecture[inputOrStore];
+    //if no store maps to this input throw error
+    if (!storeToWaitFor) {
+      throw new Error('store is waiting for an input: "' + inputOrStore + '" that no store outputs.  If this store needs no inputs than only call "itOutputs" method.');
+    }
+  } else {
+    //if inputOrStore is a store use that
+    storeToWaitFor = inputOrStore;
+  }
+  //return __registerId__ from storeToWaitFor
+  storeRegistrationId = storeToWaitFor.__registerId__;
+  if (storeRegistrationId) {
+    return storeRegistrationId;
+  } else {
+    //if store does not have __registerId__ than it has not been registered to the dispatcher so throw an error
+    throw new Error('store is waiting for a store that has not been registered to any actions.');
+  }
+};
+
+//_registerOutputToArchitecture FUNCTION: internal function, registers an output STRING key to the architecture OBJECT with the value of storeToArchitect so that the storeToArchitect can later be looked up by passing the same output STRING to itNeeds
+//@param output STRING: output STRING to which storeToArchitect will be mapped to on the architecture OBJECT
+ArchitectChain.prototype._registerOutputToArchitecture = function (output) {
+  //if this output is already registered to architecture OBJECT, throw error
+  if (architecture[output]) {
+    throw new Error('output: "' + output + '" is already registered to a store.');
+  } else {
+    //map output STRING to storeToArchitect
+    architecture[output] = this.storeToArchitect;
+  }
+};
+
+//architect FUNCTION: accepts a storeToArchitect and returns an instance of architectChain which will register the inputs and outputs for the passsed in store
+//@param storeToArchitect OBJECT: store that needs and outputs will be mapped to by the returned architectChain instance
 var architect = function (storeToArchitect) {
-  var architectChain = new ArchitectChain(storeToArchitect);
-  return architectChain;
+  return new ArchitectChain(storeToArchitect);
 };
 
 module.exports = architect;
--- src/__tests__/TuxArchitectStores-test.js
+++ src/__tests__/TuxArchitectStores-test.js
@@ -29,13 +29,14 @@ describe('architect', function () {
     architect(root1Store).itOutputs('root1Output');
   });
 
-  it('should add the __registerId__ to any store that needs what another store outputs at an array on the key of __tuxArchitecture__', function () {
+  it('should add an array of __registerId__s to any store that needs what another store outputs at the key of __tuxArchitecture__', function () {
     //create dependency chain described above
     architect(branch1Store).itNeeds('root1Output').itOutputs('branch1Output');
     architect(branch2Store).itNeeds('root1Output').itOutputs('branch2Output');
     architect(leaf11Store).itNeeds('branch1Output');
     architect(leaf12Store).itNeeds('branch1Output');
     architect(leaf21Store).itNeeds('branch2Output');
+
     //branch1 and branch2 should be architected to wait for only root1Store
     expect(branch1Store.__tuxArchitecture__[0]).toEqual(root1Store.__registerId__);
     expect(branch1Store.__tuxArchitecture__.length).toEqual(1);
@@ -58,6 +59,7 @@ describe('architect', function () {
     architect(leaf11Store).itNeeds(branch1Store);
     architect(leaf12Store).itNeeds(branch1Store);
     architect(leaf21Store).itNeeds(branch2Store);
+
     //branch1 and branch2 should be architected to wait for only root1Store
     expect(branch1Store.__tuxArchitecture__[0]).toEqual(root1Store.__registerId__);
     expect(branch1Store.__tuxArchitecture__.length).toEqual(1);
@@ -80,6 +82,7 @@ describe('architect', function () {
     architect(leaf11Store).itNeeds(branch1Store);
     architect(leaf12Store).itNeeds('branch1Output');
     architect(leaf21Store).itNeeds('branch2Output');
+
     //branch1 and branch2 should be architected to wait for only root1Store
     expect(branch1Store.__tuxArchitecture__[0]).toEqual(root1Store.__registerId__);
     expect(branch1Store.__tuxArchitecture__.length).toEqual(1);
@@ -97,10 +100,11 @@ describe('architect', function () {
 
   it('should allow a store to output multiple outputs', function () {
     //create dependency chain wherein all leaves need branch1
-    architect(branch1Store).itNeeds('root1Output').itOutputs(['branch1Output', 'branch1Output2']).and('branch1Output3');
+    architect(branch1Store).itOutputs(['branch1Output', 'branch1Output2']).and('branch1Output3');
     architect(leaf11Store).itNeeds('branch1Output');
     architect(leaf12Store).itNeeds('branch1Output2');
     architect(leaf21Store).itNeeds('branch1Output3');
+
     //leaf11, leaf12, and leaf21 should be architected to wait for only branch1
     expect(leaf11Store.__tuxArchitecture__[0]).toEqual(branch1Store.__registerId__);
     expect(leaf11Store.__tuxArchitecture__.length).toEqual(1);
@@ -114,6 +118,8 @@ describe('architect', function () {
     //create dependency chain wherein leaf21 needs both branches and the root
     architect(branch1Store).itOutputs('branch1Output');
     architect(leaf21Store).itNeeds(['branch1Output', branch2Store]).and('root1Output');
+
+    //leaf21 should be architected to wait for root1, branch1, and branch2
     expect(leaf21Store.__tuxArchitecture__[0]).toEqual(branch1Store.__registerId__);
     expect(leaf21Store.__tuxArchitecture__[1]).toEqual(branch2Store.__registerId__);
     expect(leaf21Store.__tuxArchitecture__[2]).toEqual(root1Store.__registerId__);
@@ -123,19 +129,19 @@ describe('architect', function () {
   it('should throw an error if a store is waiting for an input that no store outputs', function () {
     expect(function () {
       architect(root1Store).itNeeds('test');
-    }).toThrow(new Error('store is waiting for an input: "test" that no store outputs.  If this store needs no inputs than only call "itOutputs"'));
+    }).toThrow(new Error('store is waiting for an input: "test" that no store outputs.  If this store needs no inputs than only call "itOutputs" method.'));
   });
 
   it('should throw an error if a store is waiting for a store that does not have a __registerId__', function () {
     delete root1Store.__registerId__;
     expect(function () {
       architect(branch1Store).itNeeds('root1Output');
-    }).toThrow(new Error('store is waiting for a store that has not been registered to the dispatcher'));
+    }).toThrow(new Error('store is waiting for a store that has not been registered to any actions.'));
   });
 
-  it('a store attempts to output the same string another store is already outputing', function () {
+  it('should throw an error if a store attempts to output the same string another store is already outputing', function () {
     expect(function () {
       architect(branch1Store).itOutputs('root1Output');
-    }).toThrow(new Error('output: "root1Output" is already registered to a store'));
+    }).toThrow(new Error('output: "root1Output" is already registered to a store.'));
   });
 });
