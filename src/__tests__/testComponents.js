e414f89683e50937b91faf77a3765d106806975b
plauer <plauer411@gmail.com>
2015-01-05 20:53:39 -0800

    Create a simple Idea component for use in testing.

---
+++ src/__tests__/testComponents.js
@@ -0,0 +1,14 @@
+var React = require("react");
+
+//Creates a simple component to be used for testing
+var Idea = React.createClass({
+  render: function() {
+    return (
+      <div className="idea">
+        this.props.text
+      </div>
+    );
+  }
+});
+
+module.exports = Idea;
