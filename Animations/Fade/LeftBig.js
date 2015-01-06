199f996d769d6cfa86f65a0b10627b3a8d7adba1
spencer stebbins <sjfstebbins@gmail.com>
2015-01-05 20:04:53 -0500

    Implement Tux main animation and easings modules.

---
+++ Animations/Fade/LeftBig.js
@@ -0,0 +1,30 @@
+var Animation = require('../Animations.js');
+//Default FadeLeftBig animation component
+var FadeLeftBig = {
+  //CSS for wrapped component on entry
+  enter: {
+    'opacity': '0.01',
+    'transform': 'translateX(-2000px)',
+    'transition-duration': '.5s',
+    'transition-timing-function': 'linear'
+  },
+  //CSS for wrapped component when entry animation completes
+  enterActive: {
+    'opacity': '1',
+    'transform': 'translateX(0)'
+  },
+  //CSS for wrapped component on leave
+  leave: {
+    'opacity': '1',
+    'transform': 'translateX(0)',
+    'transition-duration': '.5s',
+    'transition-timing-function': 'linear'
+  },
+  //CSS for wrapped component when leave animation completes
+  leaveActive: {
+    'opacity': '0.01',
+    'transform': 'translateX(-2000px)'
+  }
+};
+//Use makeTransition function from main Tux Animation module to create wrapping animation componenet and pass in the default params
+module.exports = Animation.makeTransition('FadeLeftBig', FadeLeftBig);
