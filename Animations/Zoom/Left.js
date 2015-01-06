0b7303b6a730afbe15e6053e0d81f716c784757d
spencer stebbins <sjfstebbins@gmail.com>
2015-01-05 20:04:53 -0500

    #8 Tux main animation and easings modules complete. Zoom, Fade, Fly(Only normal fly) default animation modules complete as well.

---
+++ Animations/Zoom/Left.js
@@ -0,0 +1,30 @@
+var Animation = require('../Animations.js');
+//Default ZoomLeft animation component
+var ZoomLeft = {
+  //CSS for wrapped component on entry
+  enter: {
+    'opacity': '0.01',
+    'transform': 'scale(.1) translateX(-20px)',
+    'transition-duration': '.5s',
+    'transition-timing-function': 'linear'
+  },
+  //CSS for wrapped component when entry animation completes
+  enterActive: {
+    'opacity': '1',
+    'transform': 'scale(1) translateX(0)'
+  },
+  //CSS for wrapped component on leave
+  leave: {
+    'opacity': '1',
+    'transform': 'scale(1) translateX(0)',
+    'transition-duration': '.5s',
+    'transition-timing-function': 'linear'
+  },
+  //CSS for wrapped component when leave animation completes
+  leaveActive: {
+    'opacity': '0.01',
+    'transform': 'scale(.1) translateX(-20px)'
+  }
+};
+//Use makeTransition function from main Tux Animation module to create wrapping animation componenet and pass in the default params
+module.exports = Animation.makeTransition('ZoomLeft', ZoomLeft);
--- src/Tuxedo.js
+++ src/Tuxedo.js
@@ -19,8 +19,33 @@ Tux.Components = {};
 Tux.Components.Form = require('react-validating-form');
 Tux.Components.Router = require('react-router');
 
-//Tux actions
+//Tux Actions
 Tux.Actions = require('./TuxActions');
 Tux.createActionCategory = Tux.Actions.createActionCategory;
+
+//Tux Animations
+Tux.Animations = {};
+//Fade
+Tux.Animations.Fade = require('./Animations/Fade');
+Tux.Animations.Fade.Down = require('./Animations/Fade/Down');
+Tux.Animations.Fade.DownBig = require('./Animations/Fade/DownBig');
+Tux.Animations.Fade.Left = require('./Animations/Fade/Left');
+Tux.Animations.Fade.LeftBig = require('./Animations/Fade/LeftBig');
+Tux.Animations.Fade.Right = require('./Animations/Fade/Right');
+Tux.Animations.Fade.RightBig = require('./Animations/Fade/RightBig');
+Tux.Animations.Fade.Up = require('./Animations/Fade/Up');
+Tux.Animations.Fade.UpBig = require('./Animations/Fade/UpBig');
+//Zoom
+Tux.Animations.Zoom = require('./Animations/Zoom');
+Tux.Animations.Zoom.Down = require('./Animations/Zoom/Down');
+Tux.Animations.Zoom.DownBig = require('./Animations/Zoom/DownBig');
+Tux.Animations.Zoom.Left = require('./Animations/Zoom/Left');
+Tux.Animations.Zoom.LeftBig = require('./Animations/Zoom/LeftBig');
+Tux.Animations.Zoom.Right = require('./Animations/Zoom/Right');
+Tux.Animations.Zoom.RightBig = require('./Animations/Zoom/RightBig');
+Tux.Animations.Zoom.Up = require('./Animations/Zoom/Up');
+Tux.Animations.Zoom.UpBig = require('./Animations/Zoom/UpBig');
+//Fly
+Tux.Animations.Fly = require('./Animations/Fly');
 
 module.exports = Tux;
