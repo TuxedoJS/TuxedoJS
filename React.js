f11beeb3ebede082d39d1fcc2ca1ef501b8b1983
Gunnari Auvinen <gauvinen@gmail.com>
2015-01-07 14:01:24 -0500

    Expose TuxMutableClass onto React via the createMutableClass property.

--- React.js
+++ React.js
@@ -1,3 +1,4 @@
 module.exports = require('react');
 module.exports.createOwnerClass = require('./src/TuxOwnerClass');
 module.exports.createOwneeClass = require('./src/TuxOwneeClass');
+module.exports.createMutableClass = require('./src/TuxMutableClass');
