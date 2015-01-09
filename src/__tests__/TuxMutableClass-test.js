a4659c84774f0ebec143c149944e11e3cf994377
drabinowitz <dmitrirabinowitz+development@gmail.com>
2015-01-08 23:38:08 -0500

    Refactor ownerClass and mutableClass to invoke owneeClass.

--- src/__tests__/TuxMutableClass-test.js
+++ src/__tests__/TuxMutableClass-test.js
@@ -2,17 +2,15 @@
 
 var moduleToTest = '../TuxMutableClass';
 
-jest.mock('react');
 jest.dontMock(moduleToTest);
 
 describe('TuxMutableClass', function () {
-  var React, createMutableClass, tuxMutableClass, mockMutableClassProps, mockMixins, mockGetOwnerPropsMixin, mockMutableRenderMixin, mockPureRenderMixin;
+  var owneeClass, createMutableClass, tuxMutableClass, mockMutableClassProps, mockMixins, mockMutableRenderMixin, mockPureRenderMixin;
 
   beforeEach(function () {
     // Reset TuxMutableClass and mocks before each test
-    React = require('react');
+    owneeClass = require('../TuxOwneeClass');
     createMutableClass = require(moduleToTest);
-    mockGetOwnerPropsMixin = require('../TuxGetOwnerPropsMixin');
     mockMutableRenderMixin = require('../TuxMutableRenderMixin');
     mockPureRenderMixin = require('react/lib/ReactComponentWithPureRenderMixin');
     mockMixins = [{}, {}];
@@ -25,30 +23,25 @@ describe('TuxMutableClass', function () {
   });
 
   describe('createMutableClass', function () {
-    it('should invoke React.createClass with the passed in props', function () {
-      var someMockProp = React.createClass.mock.calls[0][0].someMockProp;
+    it('should invoke owneeClass with the passed in props', function () {
+      var someMockProp = owneeClass.mock.calls[0][0].someMockProp;
       expect(someMockProp).toEqual(mockMutableClassProps.someMockProp);
     });
 
-    it('should invoke React.createClass with a copy of the passed in object but not the object itself', function () {
-      var mutableClassProps = React.createClass.mock.calls[0][0];
+    it('should invoke owneeClass with a copy of the passed in object but not the object itself', function () {
+      var mutableClassProps = owneeClass.mock.calls[0][0];
       expect(mutableClassProps).not.toEqual(mockMutableClassProps);
     });
 
-    it('should add the getOwnerPropsMixin', function () {
-      var getOwnerPropsMixin = React.createClass.mock.calls[0][0].mixins[0];
-      expect(getOwnerPropsMixin).toEqual(mockGetOwnerPropsMixin);
-    });
-
     it('should add PureRenderMixin if mutableTraits is not defined', function () {
-      var pureRenderMixin = React.createClass.mock.calls[0][0].mixins[1];
+      var pureRenderMixin = owneeClass.mock.calls[0][0].mixins[0];
       expect(pureRenderMixin).toEqual(mockPureRenderMixin);
     });
 
     it('should add MutableRenderMixin if mutableTraits is defined', function () {
       mockMutableClassProps.mutableTraits = {};
       tuxMutableClass = createMutableClass(mockMutableClassProps);
-      var mutableRenderMixin = React.createClass.mock.calls[1][0].mixins[1];
+      var mutableRenderMixin = owneeClass.mock.calls[1][0].mixins[0];
       expect(mutableRenderMixin).toEqual(mockMutableRenderMixin);
     });
 
@@ -56,7 +49,7 @@ describe('TuxMutableClass', function () {
       // Add mock passed in mixins
       mockMutableClassProps.mixins = mockMixins;
       tuxMutableClass = createMutableClass(mockMutableClassProps);
-      var mixins = React.createClass.mock.calls[1][0].mixins;
+      var mixins = owneeClass.mock.calls[1][0].mixins;
       // It should not be the original mixin array but should have its properties
       expect(mixins).not.toEqual(mockMixins);
       expect(mixins[mixins.length - 2]).toEqual(mockMixins[0]);
